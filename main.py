9c17ded33232304f43e2ea6ef1f8b626a4f42ebe
Renato <renato.sousa.gc@hotmail.com>
2023-04-30 22:27:17 -0300

    feat: generate file every hundred new properties

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import pandas as pd
 from model.driver import Driver
-from bs4 import BeautifulSoup
+from selenium.common.exceptions import StaleElementReferenceException
 import time
 
 URL = 'https://www.quintoandar.com.br/alugar/imovel/sao-paulo-sp-brasil'
 PATH_WEBDRIVER = 'WebDriver/msedgedriver.exe'
 
 def main():
+
+    # Setup and start webdriver
     driver = Driver(PATH_WEBDRIVER)
     driver.start()
     driver.enter_page(URL)
+    time.sleep(4)
 
-    df_casas = pd.DataFrame()
     # Scroll navigate configuration
     element_scroll = driver.get_elements('div', 'class', 'sc-18lu2ht-0 bvIvXr')[0]
     driver.driver.execute_script("arguments[0].style.overflow = 'visible';", element_scroll)
     scroll_repeat_in_pixels = 250
 
+    # Initialize variables
+    df_properties = pd.DataFrame()
     success = 0
     error = 0
+    hundred = 100
+
+    # Loop to interact with page
     while True:
         try:
             time.sleep(1)
@@ -34,12 +41,14 @@ def main():
             df_temp['rent'] = [house[3].split('R$ ')[1] for house in house_list_temp]
             df_temp['total'] = [house[4].split('R$ ')[1] for house in house_list_temp]
 
-            df_casas = pd.concat([df_casas, df_temp]).reset_index(drop=True).drop_duplicates().reset_index(drop=True)
+            # Add the collected temporary dataframe to main dataframe
+            df_properties = pd.concat([df_properties, df_temp]).reset_index(drop=True).drop_duplicates().reset_index(drop=True)
+            total_properties = df_properties.shape[0]
+            if total_properties > hundred:
+                hundred += 100
+                df_properties.to_csv('quintoandar.csv', index=False)
+                print('File quintoandar.csv generated!')
 
-            driver.driver.execute_script('arguments[0].scrollIntoView();', driver.get_elements('div', 'data-testid', 'house-card-container')[-3])
-            ver_mais_btn = driver.get_elements('button', 'aria-label', 'Ver mais')[0]
-            time.sleep(1)
-            driver.click_element(ver_mais_btn)
             # Scroll until find "Ver mais" button
             while True:
                 driver.driver.execute_script(f"arguments[0].scrollTop += {scroll_repeat_in_pixels}", element_scroll)
@@ -52,9 +61,9 @@ def main():
                     continue
 
             success += 1
-            print('Sucesso: ' + str(success) + ' | Casas encontradas: ' + str(df_temp.shape[0]) + ' | Total de casas: ' + str(df_casas.shape[0]))
-            
-        except:
+            print('Success: ' + str(success) + ' | Properties found: ' + str(df_temp.shape[0]) + ' | Total properties: ' + str(df_properties.shape[0]))
+
+        except Exception as e:
             error += 1
             print('Erro: ' + str(error) + ' - ' + str(e))
 
